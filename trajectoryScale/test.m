db343aa992d98c52bbde930da4eca11b5bac109b
swtony <anton.porcelli@gmail.com>
2018-12-05 18:21:27 +0100

    added multi sector multi degree o freedom scaler

--- trajectoryScale/test.m
+++ trajectoryScale/test.m
@@ -3,19 +3,22 @@ clc,clear,close all;
 dt=0.01;
 time=0:dt:2;
 
-q=(time-1).^3+1;%time.^2;%(time-1).^3+1; %sin(time);
-%dq=zeros(1,size(q,2));
-%ddq=zeros(1,size(q,2));
-plot(time,q);
-hold on;
-%plot(time,dq);
-%plot(time,ddq);
+q1=(time-1).^3+1;%time.^2;%(time-1).^3+1; %sin(time);
+q2=-(time-1).^3+2;%time.^2;%(time-1).^3+1; %sin(time);
 
-%% trajectory scaling
 c=1; %scaling factor
-%[nq,ndq,nddq,ntime]=trajLinearScaler(q,dq,ddq,time,c);
+[q1,dq1,ddq1,ntime]=trajSectorScaler(q1,0,0,time,c);
+[q2,dq2,ddq2,ntime]=trajSectorScaler(q2,0,0,time,c);
 
-[nq,ndq,nddq,ntime]=trajSectorScaler(q,0,0,time,c);
+nq=[q1;q2];
+ndq=[dq1;dq2];
+nddq=[ddq1;ddq2];
+plot(time,nq);
+hold on;
+%% multi sector trajectory scaling
+waypoint=[1,51,101,151,201];
+c=[1,1.1,0.9,1.2]; %scaling factorc;
+[nq,ndq,nddq,ntime]=trajMultiSectorScaler(nq,ndq,nddq,ntime,c,waypoint);
 % 
 figure;
 plot(ntime,nq);
@@ -23,10 +26,10 @@ hold on;
 plot(ntime,ndq);
 plot(ntime,nddq);
 
-%% multi sector trajectory scaling
+%% multi sector trajectory scaling N gdl
 waypoint=[1,51,101,151,201];
 c=[1,1.1,0.9,1.2]; %scaling factorc;
-[nq,ndq,nddq,ntime]=trajMultiSectorScaler(nq,ndq,nddq,ntime,c,waypoint);
+[nq,ndq,nddq,ntime]=trajMultiSectorScalerNgdl(nq,ndq,nddq,ntime,c,waypoint);
 % 
 figure;
 plot(ntime,nq);
---
+++ trajectoryScale/trajMultiSectorScalerNgdl.m
@@ -0,0 +1,11 @@
+function [nq,ndq,nddq,ntime] = trajMultiSectorScalerNgdl(q,dq,ddq,t,scaleFactor,wypntlst)
+%TRAJMULTISECTORSCALERNGDL Summary of this function goes here
+%   Detailed explanation goes here
+nq=q;
+ndq=dq;
+nddq=ddq;
+for gdl=1:size(q,1)
+[nq(gdl,:),ndq(gdl,:),nddq(gdl,:),ntime]= trajMultiSectorScaler(q(gdl,:),dq(gdl,:),ddq(gdl,:),t,scaleFactor,wypntlst)
+end
+end
+
